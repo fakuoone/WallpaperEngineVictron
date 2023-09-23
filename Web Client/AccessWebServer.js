97f7381d2887f036f151265d8ba6018021231b5d
Fabian Kurth <fabian.kurth01@googlemail.com>
2023-09-23 19:40:29 +0200

    improved color blend function

--- Web Client/AccessWebServer.js
+++ Web Client/AccessWebServer.js
@@ -27,14 +27,35 @@ function get_temp_color(temperature) {
     // F08373 bright pastelle red and CEFF47 bright green (triade)
 }
 
-function get_soc_color(soc) {
-    let red = 255 * (1 - soc / 100);
-    let green = 255 * soc / 100;
 
-    red = red / Math.max(red, green) * 255;
-    green = green / Math.max(red, green) * 255;
+function linear_color_blend(colors,  control, control_max, control_min) {
+    // takes a list of rgb colors, a variable parameter control and its limits and blends between all colors which are distributed equally in respect to control
+    let i = 0;
+    let color_out = [0, 0, 0];
+    let color_amount = colors.length;
 
-    return 'rgb(' + red + ', '  + green + ', 0)';
+    let division_scaler = 10000;
+    let control_normalized = (control - control_min) / (control_max - control_min) * division_scaler;
+    let segment_size = division_scaler / (color_amount - 1);
+    let control_wrapped = 0;
+    let control_segment = Math.min(Math.floor(control_normalized / segment_size), (color_amount - 2));
+
+    if (control < control_max) {
+        control_wrapped = Math.min(Math.max(control_normalized % segment_size / segment_size, 0), 1);
+    }
+    else {
+        control_wrapped = 1;
+    }
+
+    for (i = 0; i < 3; i++) {
+        color_out[i] = colors[control_segment][i] + control_wrapped * (colors[control_segment + 1][i] - colors[control_segment][i]);
+    }
+
+    for (i = 0; i < 3; i++) {
+        color_out[i] = Math.round(color_out[i] / Math.max.apply(null, color_out) * 255);
+    }
+
+    return 'rgb(' + color_out[0] + ', '  + color_out[1] + ', '+ color_out[2] + ')';
 }
 
 function update_html(data_struct) {
@@ -42,14 +63,11 @@ function update_html(data_struct) {
 
     for (let i = 0; i < entries.length; i++) {
         let actual_data = Object.entries(entries[i][1]);
-        //console.log(actual_data);
-    
         let name = actual_data[0][1];
         let data = Object.entries(actual_data[1][1]);
 
         for (let y = 0; y < data.length; y++)
         {
-            //console.log('lbl' + data[y][0]);
             try {
                 document.getElementById('lbltxt' + data[y][0]).innerHTML =  data[y][0].replace('Consumption','').replace('Battery ','').replace('Leistung ','').replace('Input Voltage ','').replace('Energie', ' ').replace('Verbrauch ', '').replace('Netzbezug ', '');
                 document.getElementById('lblval' + data[y][0]).innerHTML =  data[y][1]["value"] + " " + data[y][1]["unit"];
@@ -61,9 +79,13 @@ function update_html(data_struct) {
     }
 
     let soc = Math.floor(Number(data_struct[225]["Data"]["Battery SOC"]["value"]));
+    let solar1 = Math.floor(Number(data_struct[100]["Data"]["Leistung String 1"]["value"]));
     let temp = Math.floor(Number(data_struct[24]["Data"]["Temperatur"]["value"]));
+
    
-    document.getElementById('lblvalBattery SOC').style.color = get_soc_color(soc);
+    document.getElementById('lblvalLeistung String 1').style.color = linear_color_blend([[255, 255, 255], [255, 141, 41], [255, 255, 0]], solar1, 4000, 0);
+    document.getElementById('lblvalBattery SOC').style.color = linear_color_blend([[255, 0, 0],[0, 255, 0]], soc, 0, 100);
+
     document.getElementById('lblvalTemperatur').style.color = get_temp_color(temp);
 
     if (Number(data_struct[225]["Data"]["Battery Power"]["value"] > 50)) {
